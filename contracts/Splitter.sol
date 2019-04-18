be442c39426d71e4a63ae7eb18a49bd18f1bd869
Derrick Hau <derrickhau@gmail.com>
2019-04-18 15:39:33 +0000

    Remove unused onlyEven modifier, remove account lock, zero after withdrawal, update contractPaused, update tieBreaker, move paused to parent contract

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,39 +1,49 @@
 pragma solidity ^0.5.0;
 
-contract Splitter {
+contract Paused {
+    bool contractPaused;
+    address payable alice;
+
+    event ContractPaused (bool);
+    
+    modifier onlyAlice {
+        require (msg.sender == alice, "Restricted access to Alice");
+        _;
+    }
+    modifier notPaused {
+        require (!contractPaused);
+        _;
+    }
+    
+    constructor () public {
+        alice = msg.sender;
+    }
+
+    function toggleContractPaused() public onlyAlice {
+        if (contractPaused == false) {
+            contractPaused = true;
+            emit ContractPaused (true);
+        } else {
+            contractPaused = false;
+            emit ContractPaused (false);
+        }    
+    }
+}
+
+contract Splitter is Paused {
     address payable public alice;
     address payable public bob;
     address payable public carol;
     uint bobEtherTotal;
     uint carolEtherTotal;
-    bool bobEtherLocked;
-    bool carolEtherLocked;
-    bool contractPaused;
     bool tieBreakerSwitch;
     
     event DepositReceived(uint indexed depositAmount);
     event TieBreakerResult(string winner);
-    event SplitSuccess(uint bobEtherSplit, bool bobEtherLocked, uint carolEtherSplit, bool carolEtherLocked);
+    event SplitSuccess(uint bobEtherSplit, uint carolEtherSplit);
     event WithdrawalSuccess(address indexed receiver, address sender, uint indexed amountReceived);
-    event ContractPaused (bool);
-    
-    modifier onlyAlice() {
-        require (msg.sender == alice);
-        _;
-    }
-    
-    modifier onlyEvenDeposit() {
-        require(msg.value % 2 == 0, "Please use even value");
-        _;
-    }
-    
-    modifier notPaused {
-        require (contractPaused == false);
-        _;
-    }
-    
+
     constructor (address payable _bob, address payable _carol) public {
-        alice = msg.sender;
         bob = _bob;
         require(bob != address(0));
         carol = _carol;
@@ -45,23 +55,37 @@ contract Splitter {
         splitterOfEth(msg.value);
     }
 
+    function bobEtherWithdrawal() public payable notPaused() {
+        require(bob == msg.sender, "Restricted access, Bob only");
+        require(bobEtherTotal > 0, "Insufficient funds");
+        emit WithdrawalSuccess (bob, alice, bobEtherTotal);
+        bob.transfer(bobEtherTotal);
+        bobEtherTotal = 0;
+    }
+    
+    function carolEtherWithdrawal() public payable notPaused() {
+        require(carol == msg.sender, "Restricted access, Carol only");
+        require(carolEtherTotal > 0, "Insufficient funds");
+        emit WithdrawalSuccess (carol, alice, carolEtherTotal);
+        carol.transfer(carolEtherTotal);
+        carolEtherTotal = 0;
+    } 
+
     function splitterOfEth(uint aliceEther) private onlyAlice() notPaused() {
         uint bobEtherSplit;
         uint carolEtherSplit;
         uint splitEther = aliceEther / 2;
         if (splitEther * 2 != aliceEther)
             if (tieBreaker())
-                bobEtherSplit += 1;
+                bobEtherSplit = 1;
             else
-                carolEtherSplit += 1;
+                carolEtherSplit = 1;
         bobEtherSplit += splitEther;
         carolEtherSplit += splitEther;
         require (aliceEther == bobEtherSplit + carolEtherSplit, "Split error, accounts are locked");
         bobEtherTotal += bobEtherSplit;
         carolEtherTotal += carolEtherSplit;
-        bobEtherLocked = false;
-        carolEtherLocked = false;
-        emit SplitSuccess(bobEtherSplit, bobEtherLocked, carolEtherSplit, carolEtherLocked);
+        emit SplitSuccess(bobEtherSplit, carolEtherSplit);
     }
     
     function tieBreaker() private returns (bool){
@@ -74,37 +98,5 @@ contract Splitter {
             tieBreakerSwitch = true;
             return false;
         }
-    }
-    
-    function bobEtherWithdrawal() public payable notPaused() returns (bool success) {
-        require(bob == msg.sender, "Restricted access, Bob only");
-        require(bobEtherTotal > 0, "Insufficient funds");
-        require(!bobEtherLocked, "Access locked until successful deposit is complete");
-        bobEtherLocked = true;
-        bobEtherTotal = 0;
-        bob.transfer(bobEtherTotal);
-        emit WithdrawalSuccess (bob, alice, bobEtherTotal);
-        return true;
-    }
-    
-    function carolEtherWithdrawal() public payable notPaused() returns (bool success){
-        require(carol == msg.sender, "Restricted access, Carol only");
-        require(carolEtherTotal > 0, "Insufficient funds");
-        require(!carolEtherLocked, "Access locked until successful deposit is complete");
-        carolEtherLocked = true;
-        carolEtherTotal = 0;
-        carol.transfer(carolEtherTotal);
-        emit WithdrawalSuccess (carol, alice, carolEtherTotal);
-        return true;
-    }    
-        
-    function toggleContractPaused() public onlyAlice {
-        if (contractPaused == false) {
-            contractPaused = true;
-            emit ContractPaused (true);
-        } else {
-            contractPaused = false;
-            emit ContractPaused (false);
-        }    
     }
 }
