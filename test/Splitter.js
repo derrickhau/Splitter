3274d83cd7baee845a303d92402cb3582af85e4f
Derrick Hau <derrickhau@gmail.com>
2019-04-27 06:15:05 +0000

    *Does Not Compile* Update - operator to .sub, add withdrawl test, add onlyOwner test

--- test/Splitter.js
+++ test/Splitter.js
@@ -15,27 +15,46 @@ contract("Splitter", accounts => {
 	});
 
 	it("Should split even deposit correctly", async function () {
-		const deposit = 2;
-		const preBalance1 = await instance.balances.call(recipient1);
-		console.log("preBalance1: ", preBalance1.toString());
-		const preBalance2 = await instance.balances.call(recipient2);
-		console.log("preBalance2: ", preBalance2.toString());
+		const deposit = 2; // Must be even number
 		await instance.splitFunds(recipient1, recipient2, { from: owner, value: deposit });
 		const postBalance1 = await instance.balances.call(recipient1);
-		console.log("TypeOf PostBal1: ", typeof(postBalance1));
 		const postBalance2 = await instance.balances.call(recipient2);
-		const amount1 = postBalance1 - preBalance1;
-		console.log("amount1: ", amount1);
-		const amount2 = postBalance2 - preBalance2;
-		console.log("amount2: ", amount2);
 		assert.strictEqual(amount1, amount2, "Failed to split even deposit into equal halves");
 		assert.strictEqual(amount1, deposit / 2, "Failed to split deposit into correct amount")
 	});
 
-	it("Should log amount sent to contract correctly", async function () {
+	it("Should log amount sent, from owner, to recipients, to contract correctly", async function () {
 		const deposit = 3;
 		const txObject = await instance.splitFunds(recipient1, recipient2, { from: owner, value: deposit });
 		assert.strictEqual(txObject.logs[0].args.amount.toString(), deposit, 
-				"Failed to log amount sent to contract correctly");
+			"Failed to log amount sent to contract correctly");
+		assert.strictEqual(txObject.logs[0].args.owner, account[0], 
+			"Failed to log address sent from correctly");
+		assert.strictEqual(txObject.logs[0].args.recipient1, account[1],
+			"Failed to log address sent from correctly");
+		assert.strictEqual(txObject.logs[0].args.recipient2, account[2],
+			"Failed to log address sent from correctly");
+	});
+
+	it("Should withdraw correctly", async function () {
+		const deposit = 2; // Must be even number
+		await instance.splitFunds(recipient1, recipient2, { from: owner, value: deposit });
+		let postBalance1 = await instance.balances.call(recipient1);
+		console.log("Balance before withdrawl: " postBalance1.toString(10);
+		const txObject = await instance.withdrawl({ from: recipient1 });
+		postBalance1 = await instance.balances.call(recipient1);
+		console.log("Balance after withdrawl: " postBalance1).toString(10);
+		assert.strictEqual(txObject.logs[0].args.amount.toString(10), "1",
+			"Failed to log withdrawl correctly");
+	});
+
+	it("Should protect when onlyOwner is violated", async function () {
+		const deposit = 2;		
+		assert.throws(
+			() => { instance.splitFunds(recipient1, recipient2, { from: recipient1, value: deposit }); },
+			{
+				name: 'onlyOwner Error',
+				message: 'Access restrict to owner'
+			});
 	});
 });
