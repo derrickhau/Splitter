c3f0ff84a4a359430e0ced5a5a43ea53eaf8e503
Derrick Hau <derrickhau@gmail.com>
2019-05-04 10:18:25 +0000

    *Withdraw test(4th) fails* working on gas costs

--- test/Splitter.js
+++ test/Splitter.js
@@ -23,16 +23,34 @@ contract("Splitter", accounts => {
 		assert.strictEqual(amount1.toString(), (deposit / 2).toString(), "Failed to split deposit into correct amount");
 	});
 
-	it("Should withdraw correctly", async function () {
+	it("Should log withdraw correctly and zero account balance", async function () {
 		const deposit = 2; // Must be even number
 		await instance.splitFunds(recipient1, recipient2, { from: owner, value: deposit });
 		let postBalance1 = await instance.balances.call(recipient1);
 		const txObject = await instance.withdraw({ from: recipient1 });
 		postBalance1 = await instance.balances.call(recipient1);
+		const webCallBalance = await web3.eth.getBalance(recipient1);
 		assert.strictEqual(txObject.logs[0].args.amount.toString(), (deposit / 2).toString(),
 			"Failed to log withdrawl correctly");
 		assert.strictEqual(postBalance1.toString(), "0",
 			"Failed to zero account balance after withdrawl");
+	});
+
+	it("Should withdraw correct amount to recipient1", async function () {
+		const deposit = 2000; // Must be even number
+		//const gasCost = 588380000000000; // Gas cost from previous test, but it seems to vary
+		await instance.splitFunds(recipient1, recipient2, { from: owner, value: deposit });
+		const preBalance = await web3.eth.getBalance(recipient1);
+		await instance.withdraw({ from: recipient1 });
+		const postBalance = await web3.eth.getBalance(recipient1);
+		// const withdrawl = (postBalance.toString() - preBalance.toString()); //+ gasCost.toString())
+		const withdrawl = (preBalance.toString() - postBalance.toString());
+		console.log("withdrawl: ", withdrawl);
+		console.log("deposit: ", (deposit /2).toString());
+		// const gasCost = (withdrawl - (deposit / 2).toString);
+		// console.log("gasCost: ", gasCost);
+		assert.strictEqual(withdrawl, (deposit / 2).toString(),
+			"Failed to withdraw correct amount to recipient1");
 	});
 
 	it("Should log amount sent to contract correctly", async function () {
